f93c138005e372e37a9c506b16c8f972a3e5400d
Salvatore Di Palo <dipalosalvatore1@gmail.com>
2022-08-08 16:32:29 +0200

    Add ComponentHeading component

---
+++ src/components/ComponentHeading/index.js
@@ -0,0 +1,18 @@
+import { Box, Typography } from "@mui/material";
+
+export default function ComponentHeading({ title, description }) {
+  return (
+    <Box
+      textAlign={"center"}
+      px={{ xs: 2, sm: 0 }}
+      pb={{ xs: "2rem", sm: "3rem", md: "4rem" }}
+    >
+      <h1>{title}</h1>
+      {description && (
+        <Typography variant="caption" color="secondary">
+          {description}
+        </Typography>
+      )}
+    </Box>
+  );
+}
